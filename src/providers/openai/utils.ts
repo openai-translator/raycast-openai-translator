1cd74d5fbb5e6c93abd4c72d19b0aee3d32f6f0c
tiou <dourokinga@gmail.com>
2023-03-15 21:50:35 +0800

    feat: Changing mode inplace

--- src/providers/openai/utils.ts
+++ src/providers/openai/utils.ts
@@ -29,7 +29,7 @@ export async function fetchSSE(input: string, options: FetchSSEOptions) {
         }
       }
     }
-  }catch(error) {
-    onError({error})
+  } catch (error) {
+    onError({ error });
   }
 }
