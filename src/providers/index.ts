62a06945a88658af1558260d40d27d4d8687e838
douo <dourokinga@gmail.com>
2023-05-23 19:04:01 +0800

    chore: update to the latest raycast extension specification.

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -6,23 +6,24 @@ import openai from "./openai";
 import raycast from "./raycast";
 import azure from "./azure";
 import palm2 from "./palm2";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-const PROVIDER_CLASSES : Record<string, new (...args: any[]) => Provider> = {
-  "openai": openai,
-  "raycast": raycast,
-  "azure": azure,
-  "palm2": palm2
-}
+const PROVIDER_CLASSES: Record<string, new (...args: any[]) => Provider> = {
+  openai: openai,
+  raycast: raycast,
+  azure: azure,
+  palm2: palm2,
+};
 
-const record: Record<string, Provider> = {}
+const record: Record<string, Provider> = {};
 export function getProvider(provider: string): Provider {
   if (!(provider in record)) {
     const preferences = getPreferenceValues<{
-    entrypoint: string;
-    apikey: string;
-    apiModel: string;
-    }>()
-    const providerClass = PROVIDER_CLASSES[provider]
+      entrypoint: string;
+      apikey: string;
+      apiModel: string;
+    }>();
+    const providerClass = PROVIDER_CLASSES[provider];
     record[provider] = new providerClass(preferences);
   }
   return record[provider];
--- src/providers/lang_recognizer_wrapper.ts
+++ src/providers/lang_recognizer_wrapper.ts
@@ -4,16 +4,16 @@ import fs from "fs";
 
 const binary = `${environment.assetsPath}/lang`;
 
-export default async function detect(text:string): Promise<string> {
-  const { status, output, stdout, stderr, error } = spawnSync(binary, [text])
+export default async function detect(text: string): Promise<string> {
+  const { status, stdout, stderr } = spawnSync(binary, [text]);
   try {
     await fs.promises.access(binary, fs.constants.X_OK);
   } catch {
     await fs.promises.chmod(binary, 0o775);
   }
-  if(status !=0){
-    throw new Error(stderr.toString())
-  }else{
-    return stdout.toString()
+  if (status != 0) {
+    throw new Error(stderr.toString());
+  } else {
+    return stdout.toString();
   }
 }
